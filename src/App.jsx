db0201ebb139c842e3e37e7501a0796abed5016d
t46864751 <t46864751@gmail.com>
2026-02-06 12:21:46 +0000

    fix: Add basename to BrowserRouter for correct routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/take-a-rain">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/getweather" element={<GetWeather />} />
